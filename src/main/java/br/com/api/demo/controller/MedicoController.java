b3b95f6fb79d469abd22ea701853acb756d31266
Ranon Emerick <ranonemerick@gmail.com>
2024-02-21 17:48:48 -0300

    Atualizaco nas Classes Medico e Endereco

--- src/main/java/br/com/api/demo/controller/MedicoController.java
+++ src/main/java/br/com/api/demo/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package br.com.api.demo.controller;
 
+import br.com.api.demo.endereco.Endereco;
 import br.com.api.demo.medico.DadosCadastroMedico;
+import br.com.api.demo.medico.Medico;
+import br.com.api.demo.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
