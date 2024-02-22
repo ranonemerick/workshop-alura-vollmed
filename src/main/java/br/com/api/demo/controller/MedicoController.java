a3c475dedffee51ef962d1972813bcd6e40ecdf4
Ranon Emerick <ranonemerick@gmail.com>
2024-02-22 10:30:59 -0300

    Criacao do Metodo Listagem de Medico + DTO DadosListagemMedico

--- src/main/java/br/com/api/demo/controller/MedicoController.java
+++ src/main/java/br/com/api/demo/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package br.com.api.demo.controller;
 
 import br.com.api.demo.medico.DadosCadastroMedico;
+import br.com.api.demo.medico.DadosListagemMedico;
 import br.com.api.demo.medico.Medico;
 import br.com.api.demo.medico.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -21,6 +21,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
