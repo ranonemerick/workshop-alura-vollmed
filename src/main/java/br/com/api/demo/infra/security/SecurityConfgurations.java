2432c7b0ecc8f3b8822c412ba08116bb7fdc3ed5
Ranon Emerick <ranonemerick@gmail.com>
2024-02-23 11:17:05 -0300

    Ajuste na Classe SecurityConfiguration + Metodo AutenticationManager

--- src/main/java/br/com/api/demo/infra/security/SecurityConfgurations.java
+++ src/main/java/br/com/api/demo/infra/security/SecurityConfgurations.java
@@ -2,6 +2,8 @@ package br.com.api.demo.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,9 +14,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfgurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 
 }
