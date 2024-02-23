ac505196079cb03518c2d9975086fbe6ba6081e5
Ranon Emerick <ranonemerick@gmail.com>
2024-02-23 15:58:42 -0300

    Ajuste na Classe SecurityFilter

--- src/main/java/br/com/api/demo/infra/security/SecurityFilter.java
+++ src/main/java/br/com/api/demo/infra/security/SecurityFilter.java
@@ -14,6 +14,16 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null) {
+            throw new RuntimeException("Token não enviado");
+        }
+        return authorizationHeader.replace("Bearer", "");
     }
 }
