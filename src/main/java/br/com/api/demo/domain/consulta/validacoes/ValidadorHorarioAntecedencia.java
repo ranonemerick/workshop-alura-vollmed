e93c30bed196df19b29d8d343955461cc2c62cfa
Ranon Emerick <ranonemerick@gmail.com>
2024-02-26 15:46:35 -0300

    Adicionada a Classe Para Validar Tempo de Antecedencia Para Agendamento de Consultas

---
+++ src/main/java/br/com/api/demo/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,20 @@
+package br.com.api.demo.domain.consulta.validacoes;
+
+import br.com.api.demo.domain.ValidacaoException;
+import br.com.api.demo.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, dataConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30) {
+            throw new ValidacaoException("Agendamento apenas com 30 minutos de antecedência!");
+        }
+    }
+}
